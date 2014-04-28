eb605766f36bbdb358c98cb8619eb0519e0a4f38
sprabina <sprabina@Prabinas-MacBook-Pro.local>
2014-04-27 19:14:17 -0500

    PShrestha: CRUD operations for Person and Location

--- src/com/msse/teamflyte/affinitymapper/AffinityMapperController.java
+++ src/com/msse/teamflyte/affinitymapper/AffinityMapperController.java
@@ -1,24 +1,154 @@
 package com.msse.teamflyte.affinitymapper;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
 import com.google.api.server.spi.config.Named;
-import com.google.appengine.api.users.User;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @Api(name = "affinitymapper", version = "v1")
 public class AffinityMapperController {
 
+	@SuppressWarnings("unchecked")
 	@ApiMethod(name = "getUser", path = "user/{userId}", httpMethod = HttpMethod.GET)
-	public Map<String, String> getUser(@Named("userId") String userId, User user, HttpServletRequest req) {
+	public Person getUser(@Named("userId") String userId) {
+		EntityManager mgr = getEntityManager();
+		try {
+			String queryStr = "select from Person as Person where email = :email ";
+			Query query = mgr.createQuery(queryStr);
+			query.setParameter("email", userId);
+			if (query.getResultList().size() > 0) {
+				return ((List<Person>) query.getResultList()).get(0);
+			}
+
+		} finally {
+			mgr.close();
+		}
+
+		return null;
+	}
+
+	@ApiMethod(name = "addUser", path = "user/add", httpMethod = HttpMethod.POST)
+	public void addUser(Person requestBody) {
+		EntityManager mgr = getEntityManager();
+		try {
+			Person person = new Person();
+			person.setEmail(requestBody.getEmail());
+			person.setName(requestBody.getName());
+			person.setChatRequestToggle(requestBody.isChatRequestToggle());
+			person.setProximityAlertLimit(requestBody.getProximityAlertLimit());
+			person.setProximityAlertToggle(requestBody.isProximityAlertToggle());
+			person.setInterestGroups(requestBody.getInterestGroups());			
+			mgr.persist(person);
+		} finally {
+			mgr.close();
+		}
+	}
 	
-		Map<String, String> result = new HashMap<String, String>();
-		result.put("name", "Prabina");
-		return result;
-	}	
+	@ApiMethod(name = "UpdateUser", path = "user/{userId}", httpMethod = HttpMethod.PUT)
+	public void updateUser(@Named("userId") String userId, Person requestBody) {
+		
+		EntityManager mgr = getEntityManager();
+		try {
+			String queryStr = "select from Person as Person where email = :email ";
+			Query query = mgr.createQuery(queryStr);
+			query.setParameter("email", userId);
+
+			if (query.getResultList().size() > 0) {
+				Person person = (Person) query.getResultList().get(0);
+				person.setName(requestBody.getName());
+				person.setChatRequestToggle(requestBody.isChatRequestToggle());
+				person.setProximityAlertLimit(requestBody.getProximityAlertLimit());
+				person.setProximityAlertToggle(requestBody.isProximityAlertToggle());
+				person.setInterestGroups(requestBody.getInterestGroups());
+				mgr.persist(person);
+			}
+
+		} finally {
+			mgr.close();
+		}
+	}
+	
+	@ApiMethod(name = "updateLocation", path = "location/user/{userId}", httpMethod = HttpMethod.POST)
+	public void updateLocation(@Named("userId") String userId, Location requestBody) {
+		EntityManager mgr = getEntityManager();
+		try {
+			String queryStr = "select from Person as Person where email = :email ";
+			Query query = mgr.createQuery(queryStr);
+			query.setParameter("email", userId);
+
+			if (query.getResultList().size() > 0) {
+				Person person = (Person) query.getResultList().get(0);
+				
+				String locationQueryStr = "select from Location as Location where email = :email ";
+				Query locationQuery = mgr.createQuery(locationQueryStr);
+				locationQuery.setParameter("email", userId);
+				
+				Location location = null;
+				if (locationQuery.getResultList().size() > 0){
+					 location = (Location) locationQuery.getResultList().get(0);
+				}else
+				{
+					String uuid = UUID.randomUUID().toString();
+					Key key = KeyFactory.createKey(person.getId(), Location.class.getSimpleName(), uuid);
+					location = new Location();
+					location.setId(key);
+					location.setEmail(userId);
+				}
+				location.setActive(true);
+				location.setLatitude(requestBody.getLatitude());
+				location.setLongitude(requestBody.getLongitude());
+				mgr.persist(location);
+			}		
+			
+		} finally {
+			mgr.close();
+		}
+	}
+	
+	@ApiMethod(name = "getLocation", path = "location/user/{userId}", httpMethod = HttpMethod.GET)
+	public Location getLocation(@Named("userId") String userId) {
+		EntityManager mgr = getEntityManager();
+		try {
+			String queryStr = "select from Person as Person where email = :email ";
+			Query query = mgr.createQuery(queryStr);
+			query.setParameter("email", userId);
+
+			if (query.getResultList().size() > 0) {
+				String locationQueryStr = "select from Location as Location where email = :email ";
+				Query locationQuery = mgr.createQuery(locationQueryStr);
+				locationQuery.setParameter("email", userId);
+				
+				if (locationQuery.getResultList().size() > 0){
+					return ((Location) locationQuery.getResultList().get(0));
+				}
+			}		
+			
+		} finally {
+			mgr.close();
+		}
+		
+		return null;
+	}
+
+	@ApiMethod(name = "listInterestGroups", path = "listInterestGroups", httpMethod = HttpMethod.GET)
+	public List<String> listInterestGroups() {
+		List<String> response = new ArrayList<String>();
+		for (InterestEnum interest : InterestEnum.values()) {
+			response.add(interest.getDisplayName());
+		}
+		return response;
+	}
+
+	private static EntityManager getEntityManager() {
+		return EMF.get().createEntityManager();
+	}
 }
--- src/com/msse/teamflyte/affinitymapper/AffinityMapperServlet.java
+++ src/com/msse/teamflyte/affinitymapper/AffinityMapperServlet.java
@@ -1,26 +1,27 @@
 package com.msse.teamflyte.affinitymapper;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
-import javax.servlet.http.*;
-
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class AffinityMapperServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException 
 	{
-		DatastoreService  ds = DatastoreServiceFactory.getDatastoreService();
+
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world Now");
 		
-		Entity user = new Entity("User","prabinashrestha@gmail.com");
-		user.setProperty("Name","Prabina Shrestha");
-		user.setProperty("email","prabinashrestha@gmail.com");		
-		ds.put(user);
+		//snippets:
+		
+//		DatastoreService  ds = DatastoreServiceFactory.getDatastoreService();		
+//		Entity user = new Entity("User","prabinashrestha@gmail.com");
+//		user.setProperty("Name","Prabina Shrestha");
+//		user.setProperty("email","prabinashrestha@gmail.com");		
+//		ds.put(user);
 //				
 //		Entity interest1 =new Entity("Interest");
 //		Entity interest2 =new Entity("Interest");
@@ -28,10 +29,12 @@ public class AffinityMapperServlet extends HttpServlet {
 //		
 //		List<Entity> interestsList = Arrays.asList(interest1, interest2, interest3);
 //		ds.put(interestsList);
+			
 		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-		
+		//CollectionResponse<Person>
+		//return CollectionResponse.<ContactOutput> builder().setItems(list).build();
+
+		//mgr.refresh(p);
 		
 	}
 }
